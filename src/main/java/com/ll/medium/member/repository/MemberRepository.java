372676359438bd59911e15c453deb48c0e3f4cb9
tseop <77163842+seop-kim@users.noreply.github.com>
2023-12-05 16:40:57 +0900

    feat: Add find nickname and username function in memberRepository

--- src/main/java/com/ll/medium/member/repository/MemberRepository.java
+++ src/main/java/com/ll/medium/member/repository/MemberRepository.java
@@ -1,7 +1,11 @@
 package com.ll.medium.member.repository;
 
 import com.ll.medium.member.entity.Member;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByUserName(String userName);
+    Optional<Member> findByNickName(String nickName);
+
 }
