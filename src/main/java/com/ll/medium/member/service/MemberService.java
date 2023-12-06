aa1a0184d8dd535a3952617681905552633a59af
tseop <77163842+seop-kim@users.noreply.github.com>
2023-12-06 09:44:35 +0900

    feat: Add function in memberService

--- src/main/java/com/ll/medium/member/service/MemberService.java
+++ src/main/java/com/ll/medium/member/service/MemberService.java
@@ -14,10 +14,10 @@ public class MemberService {
     private final PasswordEncoder passwordEncoder;
 
     // == 회원가입 ==
-    public Member join(String nickName, String userName, String password) {
+    public Member join(String nickname, String username, String password) {
         Member member = Member.builder()
-                .nickName(nickName)
-                .username(userName)
+                .nickName(nickname)
+                .username(username)
                 .password(passwordEncoder.encode(password))
                 .build();
 
@@ -27,15 +27,31 @@ public class MemberService {
 
 
     // == id 조회 ==
+    public Member findById(Long id) {
+        Optional<Member> findOne = memberRepository.findById(id);
+
+        if (findOne.isEmpty()) {
+            throw new IllegalArgumentException("검색하는 id의 사용자가 없습니다.");
+        }
+
+        return findOne.get();
+    }
 
     // == nickName 조회 ==
-    public Member findByNickName(String nickName) {
-        Optional<Member> findOne = memberRepository.findByNickName(nickName);
+    public Member findByNickname(String nickname) {
+        System.out.println("nickName : " + nickname);
+        Optional<Member> findOne = memberRepository.findByNickname(nickname);
+        System.out.println("nickName2 : "  + findOne.get().getNickname());
 
         if (findOne.isEmpty()) {
             throw new IllegalArgumentException("검색하는 닉네임의 사용자가 없습니다.");
         }
 
         return findOne.get();
+    }
+
+    // == member count ==
+    public long count() {
+        return memberRepository.count();
     }
 }
