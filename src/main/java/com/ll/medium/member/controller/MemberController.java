316a943efedf42f86a3cf2f787483fc40395b415
tseop <77163842+seop-kim@users.noreply.github.com>
2023-12-11 18:45:42 +0900

    feat: Add join mapping in memberController

--- src/main/java/com/ll/medium/member/controller/MemberController.java
+++ src/main/java/com/ll/medium/member/controller/MemberController.java
@@ -1,7 +1,12 @@
 package com.ll.medium.member.controller;
 
+import com.ll.medium.member.service.MemberService;
+import com.ll.medium.member.util.MemberJoinForm;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,18 +16,44 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequiredArgsConstructor
 @Controller
 public class MemberController {
+    private final MemberService memberService;
 
     // == 회원가입 폼 ==
     @GetMapping("/join")
-    public String joinForm() {
+    public String joinForm(MemberJoinForm memberJoinForm) {
         return "/member/joinForm";
     }
 
     // == 회원가입 ==
     @PostMapping("/join")
-    @ResponseBody
-    public String join() {
-        return "회원가입 처리가 필요합니다.";
+    public String join(@Valid MemberJoinForm memberJoinForm,
+                       BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            return "/member/joinForm";
+        }
+
+        if (!memberJoinForm.getPassword().equals(memberJoinForm.getPasswordConfirm())) {
+            bindingResult.rejectValue("passwordConfirm", "passwordInCorrect", "패스워드가 일치하지 않습니다.");
+            return "/member/joinForm";
+        }
+
+        try {
+            memberService.join(
+                    memberJoinForm.getNickname(),
+                    memberJoinForm.getUsername(),
+                    memberJoinForm.getPassword());
+
+        } catch (DataIntegrityViolationException e) {
+            bindingResult.reject("signupFailed", "이미 등록된 닉네임입니다.");
+            return "/member/joinForm";
+
+        } catch (Exception e) {
+            bindingResult.reject("signupFailed", e.getMessage());
+            return "/member/joinForm";
+        }
+
+        return "/member/login";
     }
 
     // == 로그인 폼 ==
