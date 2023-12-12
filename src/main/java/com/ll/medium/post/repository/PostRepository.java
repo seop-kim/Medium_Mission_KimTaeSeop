ec0d3727a0ec8eca093e5e7e51653e9b04773491
tseop <77163842+seop-kim@users.noreply.github.com>
2023-12-12 15:43:49 +0900

    feat: Add search function

--- src/main/java/com/ll/medium/post/repository/PostRepository.java
+++ src/main/java/com/ll/medium/post/repository/PostRepository.java
@@ -1,6 +1,7 @@
 package com.ll.medium.post.repository;
 
 import com.ll.medium.post.entity.Post;
+import java.util.List;
 import org.aspectj.weaver.patterns.TypePatternQuestions.Question;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,4 +10,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface PostRepository extends JpaRepository<Post, Long> {
     Page<Post> findAll(Pageable pageable);
     Page<Post> findByAuthor_Id(Long id, Pageable pageable);
+    Page<Post> findByTitleContaining(String keyword, Pageable pageable);
 }
