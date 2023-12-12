6607f41656ec7bcb87ed637854747fc38f3cd123
tseop <77163842+seop-kim@users.noreply.github.com>
2023-12-12 15:51:34 +0900

    Revert "Merge branch 'first' into feat/postService"
    This reverts commit b63e52225eef967d37fa1b708a6faa3cfcfff0a8, reversing
    changes made to ec0d3727a0ec8eca093e5e7e51653e9b04773491.

---
+++ src/main/java/com/ll/medium/post/util/PostForm.java
@@ -0,0 +1,15 @@
+package com.ll.medium.post.util;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class PostForm {
+    @NotEmpty(message = "제목은 필수 항목입니다.")
+    @Size(max = 200)
+    private String title;
+
+    @NotEmpty(message = "내용은 필수 항목입니다.")
+    private String content;
+}
