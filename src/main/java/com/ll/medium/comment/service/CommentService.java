0ee4d1414f164fc2bd46dd4865565b55a0b78842
tseop <77163842+seop-kim@users.noreply.github.com>
2023-12-14 01:53:35 +0900

    feat: Add comment function

---
+++ src/main/java/com/ll/medium/comment/service/CommentService.java
@@ -0,0 +1,50 @@
+package com.ll.medium.comment.service;
+
+import com.ll.medium.comment.entity.Comment;
+import com.ll.medium.comment.repository.CommentRepository;
+import com.ll.medium.member.entity.Member;
+import com.ll.medium.post.entity.Post;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class CommentService {
+    private final CommentRepository commentRepository;
+
+    public void write(String content, Member member, Post post) {
+        Comment comment = Comment.builder()
+                .content(content)
+                .author(member)
+                .post(post)
+                .build();
+        commentRepository.save(comment);
+    }
+
+    public Comment findById(Long id) {
+        Optional<Comment> findOne = commentRepository.findById(id);
+
+        if (findOne.isEmpty()) {
+            log.error("[CommentServiceException] answer not found");
+            throw new IllegalArgumentException("[CommentServiceException] answer not found");
+        }
+
+        return findOne.get();
+    }
+
+    @Transactional
+    public void editComment(Long id, String content) {
+        Optional<Comment> optionalComment = commentRepository.findById(id);
+        if (optionalComment.isPresent()) {
+            Comment comment = optionalComment.get();
+            comment.editContent(content);
+        } else {
+            // 댓글이 존재하지 않을 경우 예외 처리 또는 적절한 로직 수행
+            throw new IllegalArgumentException("댓글이 존재하지 않습니다. id=" + id);
+        }
+    }
+}
--- src/main/java/com/ll/medium/util/init/NotProd.java
+++ src/main/java/com/ll/medium/util/init/NotProd.java
@@ -1,8 +1,10 @@
 package com.ll.medium.util.init;
 
+import com.ll.medium.comment.service.CommentService;
 import com.ll.medium.member.entity.Member;
 import com.ll.medium.member.role.MemberRole;
 import com.ll.medium.member.service.MemberService;
+import com.ll.medium.post.entity.Post;
 import com.ll.medium.post.service.PostService;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
@@ -14,22 +16,34 @@ import org.springframework.context.annotation.Profile;
 public class NotProd {
 
     @Bean
-    public ApplicationRunner initNotProdData(MemberService memberService, PostService postService) {
+    public ApplicationRunner initNotProdData(MemberService memberService, PostService postService,
+                                             CommentService commentService) {
         return args -> {
+            Member member = null;
+            Post post = null;
 
             if (memberService.count() == 0) {
-                Member findOne = memberService.join("admin", "관리자", "1234");
-                findOne.roleUpdate(MemberRole.ADMIN);
+                member = memberService.join("admin", "관리자", "1234");
+                member.roleUpdate(MemberRole.ADMIN);
 
                 memberService.join("member", "김태섭", "1234");
             }
 
             if (postService.count() == 0) {
                 for (int i = 1; i <= 100; i++) {
-                    postService.create("test title " + i, "test content " + i,true, memberService.findById(1L));
+                    postService.create("test title " + i, "test content " + i, true, member);
                 }
 
-                postService.create("member test title ", "member test content ",false, memberService.findById(2L));
+                member = memberService.findById(2L);
+                postService.create("member test title ", "member test content ", false, member);
+            }
+
+            post = postService.findById(100L);
+
+            if (post != null) {
+                for (int i = 1; i <= 10; i++) {
+                    commentService.write("test comment" + i, member, post);
+                }
             }
 
         };
