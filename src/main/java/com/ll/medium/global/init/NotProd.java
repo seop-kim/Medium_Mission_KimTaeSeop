ecad8ba9ccaec7905b88dc3d9e673c532ae52d41
tseop <77163842+seop-kim@users.noreply.github.com>
2024-01-04 14:41:46 +0900

    TestData 수정

--- src/main/java/com/ll/medium/global/init/NotProd.java
+++ src/main/java/com/ll/medium/global/init/NotProd.java
@@ -1,5 +1,6 @@
 package com.ll.medium.global.init;
 
+import ch.qos.logback.core.joran.conditional.IfAction;
 import com.ll.medium.member.member.entity.Member;
 import com.ll.medium.member.member.service.MemberService;
 import com.ll.medium.post.domain.entity.Post;
@@ -31,7 +32,9 @@ public class NotProd {
     @Order(3)
     public ApplicationRunner initNotProd() {
         return args -> {
-            if (memberService.findByUsername("user1").isPresent()) return;
+            if (memberService.findByUsername("user1").isPresent()) {
+                return;
+            }
 
             self.work1();
         };
@@ -39,33 +42,19 @@ public class NotProd {
 
     @Transactional
     public void work1() {
-        Member memberUser1 = memberService.join("user1", "1234").getData();
-        Member memberUser2 = memberService.join("user2", "1234").getData();
-        Member memberUser3 = memberService.join("user3", "1234").getData();
-        Member memberUser4 = memberService.join("user4", "1234").getData();
-
-        Post post1 = postService.write(memberUser1, "제목 1", "내용 1", true, false);
-        Post post2 = postService.write(memberUser1, "제목 2", "내용 2", true, false);
-        Post post3 = postService.write(memberUser1, "제목 3", "내용 3", false, false);
-        Post post4 = postService.write(memberUser1, "제목 4", "내용 4", true, false);
-
-        Post post5 = postService.write(memberUser2, "제목 5", "내용 5", true, false);
-        Post post6 = postService.write(memberUser2, "제목 6", "내용 6", false, false);
-
-        IntStream.rangeClosed(7, 50).forEach(i -> {
-            postService.write(memberUser3, "제목 " + i, "내용 " + i, true, false);
+        IntStream.rangeClosed(1, 100).forEach(i -> {
+            Member member = memberService.join("user" + i, "1234").getData();
+            if (i % 2 == 0) {
+                member.setPaid(true);
+            }
         });
 
-        Post post7 = postService.write(memberUser2, "제목 51", "내용 51", true, false);
-
-        memberService.membershipUpdate(memberUser1.getId(), true);
-        postService.like(memberUser2, post1);
-        postService.like(memberUser3, post1);
-        postService.like(memberUser4, post1);
-
-        postService.like(memberUser2, post2);
-        postService.like(memberUser3, post2);
-
-        postService.like(memberUser2, post3);
+        IntStream.rangeClosed(1, 100).forEach(i -> {
+            Member member = memberService.findByUsername("user" + i).get();
+            Post write = postService.write(member, "제목 " + i, "내용 " + i, true, false);
+            if (i % 2 == 0) {
+                write.setPaid(true);
+            }
+        });
     }
 }
